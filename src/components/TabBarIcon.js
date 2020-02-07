53d207bc06f38799a81acdbdcd0f47ce1c5d040d
guifs <guifs100@gmail.com>
2020-02-07 00:09:33 -0300

    teste teste teste teste

---
+++ src/components/TabBarIcon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export default function TabBarIcon(props) {
+  return (
+    <Icon
+      name={props.name}
+      size={26}
+      style={{marginBottom: -3}}
+      color={props.focused ? '#28F' : '#DDD'}
+    />
+  );
+}
