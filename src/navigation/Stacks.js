084d8720141854ee4d32d14e753dc9ecef4d9f5b
iDownward <matheus-1199@hotmail.com>
2020-05-16 16:38:57 -0300

    Login stack if not logged

--- src/navigation/Stacks.js
+++ src/navigation/Stacks.js
@@ -97,7 +97,7 @@ export function LoginStack() {
   );
 }
 
-export function NewAppointmentStack() {
+export function NewAppointmentStack(props) {
   return (
     <Navigator
       initialRouteName={SERVICE_SCREEN}
@@ -120,8 +120,8 @@ export function NewAppointmentStack() {
       />
       <Screen
         name={SUMMARY_SCREEN}
-        component={SummaryScreen}
-        options={{ title: 'Confirmar agendamento' }}
+        component={props.isLogged ? SummaryScreen : LoginStack}
+        options={{ title: texts["summary:header"], headerShown: props.isLogged }}
       />
     </Navigator>
   );
