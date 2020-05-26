8762cb5a59db51e4d3441f0a2e4c6d464c1204d4
Guifs <guifs100@gmail.com>
2020-05-26 20:48:11 -0300

    Perfil com com inputs nome, email, password e radio button

--- src/pages/profile/Profile.screen.js
+++ src/pages/profile/Profile.screen.js
@@ -1,24 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, TextInput, ImageBackground } from 'react-native';
+import RadioButton from '../../components/radioButton';
 
 import styles from './Profile.styles';
 
+const PROP = [
+	{
+		key: 'samsung',
+		text: 'Samsung',
+	},
+	{
+		key: 'apple',
+		text: 'Apple',
+	},
+	{
+		key: 'motorola',
+		text: 'Motorola',
+	},
+	{
+		key: 'lenovo',
+		text: 'Lenovo',
+  },
+];
+
 const Profile = () => {
-    return (
-      <View>
-        <ImageBackground source={require('../../img/Background.jpg')} style={styles.background}>
-          {/* TODO criar uma view branca arredondada e transparente para colocar os outros campos */}
-          {/* TODO input text email, data de nascimento, nome, sexo */}
-          {/* TODO criar um text input com titulo (placeholder) */}
-          <TextInput>
+  // TODO: receber os dados de user e as outras
+  const [name, setName] = useState('')
+  const [login, setLogin] = useState('')
+	const [password, setPassword] = useState('')
+	const [loading, setLoading] = useState(false)
 
+  return (
+    <View>
+      <ImageBackground source={require('../../img/Background.jpg')} style={styles.background}>
+        {/* TODO input text login, name, password, birthDate, gender */}
+        <View style={styles.container_inputs}>
+          <Text style={ styles.text }> Nome: </Text>
+          <TextInput style={styles.input}
+          onChangeText={(name) => setName(name)}
+          placeholder= 'Digite seu name'
+          // placeholderTextColor= '#'
+          returnKeyType= 'next'
+          // keyboardType= 'email-address'
+          // autoCapitalize= 'none'
+          // autoCorrect= {false}
+          // onSubmitEditing ={() => this.passwordInput.focus()}
+          />
+          <Text style={ styles.text }> Email: </Text>
+          <TextInput style={styles.input}
+            onChangeText={(login) => setLogin(login)}
+            placeholder= 'Digite seu email'
+            // placeholderTextColor= '#'
+            returnKeyType= 'next'
+            keyboardType= 'email-address'
+            autoCapitalize= 'none'
+            autoCorrect= {false}
+            // onSubmitEditing ={() => this.passwordInput.focus()}
+          />
+          <Text style={ styles.text }> Senha: </Text>
+          <TextInput style={styles.input}
+            onChangeText={(password) => setPassword(password)}
+            placeholder= 'Digite sua senha'
+            // placeholderTextColor= '#'
+            returnKeyType= 'go'
+            secureTextEntry
+            // TODO campo email mandar para o de senha
+            // ref={(input) => this.passwordInput = input}
+          />
+				</View>
+        {/* TODO input text com a mascara de data */}
 
-          </TextInput>
-          {/* TODO botao logout */}
-          <TouchableOpacity style={styles.button} >Logout</TouchableOpacity>
-        </ImageBackground>
-      </View>
-    );
-  };
+        <View style={styles.container}>
+        <RadioButton PROP={PROP} />
+        </View>
+        {/* TODO botao logout funcionar */}
+        <TouchableOpacity style={styles.button} >
+          <Text> Logout </Text>
+        </TouchableOpacity>
+      </ImageBackground>
+    </View>
+  )
+}
   
   export default Profile;
--- src/pages/profile/Profile.styles.js
+++ src/pages/profile/Profile.styles.js
@@ -1,13 +1,10 @@
 import { StyleSheet } from 'react-native';
 import { colors } from '@utils';
 
+// TODO: ver se vai usar todos esses styles
+
 const styles = StyleSheet.create({
-  // TODO ver se vai ter logo
-  logo:{
-    width: 50,
-    height: 50
-  },
-  container:{
+	container:{
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
@@ -16,8 +13,22 @@ const styles = StyleSheet.create({
 		resizeMode: "cover",
 		width: '100%',
 		height: '100%',
-  },
-  button:{
+	},
+	container_inputs:{
+		marginTop: 20,
+		width: 250,
+		alignItems: 'center',
+	},
+	container_buttons:{
+		justifyContent: 'space-between',
+	},
+	input:{
+		width: '100%',
+		backgroundColor: colors.inputBackground,
+		margin: 5,
+		borderRadius: 10,
+	},
+	button:{
 		margin: 10,
 		marginHorizontal: 20,
 		padding: 10,
@@ -25,8 +36,33 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 		borderRadius: 10,
+
 	},
-  
+	button_register:{
+		margin: 10,
+		marginHorizontal: 20,
+		padding: 10,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 10,
+		borderWidth: 2,
+		borderColor: colors.primary,
+
+	},
+	button_text:{
+		color: colors.accent,
+		fontWeight: 'bold',
+	},
+	text:{
+		color: colors.accent,
+		fontWeight: 'bold',
+	},
+	text_error:{
+		color: colors.primary,
+		fontWeight: 'bold',
+		fontSize: 18,
+	}
+
 })
-  
+
 export default styles;
