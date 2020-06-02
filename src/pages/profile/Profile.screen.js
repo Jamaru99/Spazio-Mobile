1c4eefdc3caec0ce2a6dc146a9df9a4cb4a2fa75
Guifs <guifs100@gmail.com>
2020-06-02 18:00:16 -0300

    remoção do radio button e radio button pelo react-native-paper

--- src/pages/profile/Profile.screen.js
+++ src/pages/profile/Profile.screen.js
@@ -1,40 +1,70 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, TextInput, ImageBackground } from 'react-native';
-import RadioButton from '../../components/radioButton';
-
+import { View, 
+         Text, 
+         TouchableOpacity,
+         TextInput, 
+         ImageBackground 
+         } from 'react-native';
+import { TextField,
+         FilledTextField,
+         OutlinedTextField
+         } from 'react-native-material-textfield'
+import { RadioButton } from 'react-native-paper'
 import styles from './Profile.styles';
 
 const PROP = [
 	{
-		key: 'samsung',
-		text: 'Samsung',
+		key: 'masculino',
+		text: 'Masculino',
 	},
 	{
-		key: 'apple',
-		text: 'Apple',
+		key: 'feminino',
+		text: 'Feminino',
 	},
 	{
-		key: 'motorola',
-		text: 'Motorola',
+		key: 'outro',
+		text: 'Outro',
 	},
-	{
-		key: 'lenovo',
-		text: 'Lenovo',
-  },
 ];
+
 
 const Profile = () => {
   // TODO: receber os dados de user e as outras
   const [name, setName] = useState('')
   const [login, setLogin] = useState('')
 	const [password, setPassword] = useState('')
-	const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [birthDate, setBirthDate] = useState('')
+  
+  const [gender, setGender] = useState('first')
+  
+
+  fieldRef = React.createRef();
+
+  onSubmit = () => {
+    let { current: field } = this.fieldRef;
+
+    console.log(field.value());
+  };
+
+  formatText = (text) => {
+    return text.replace(/[^+\d]/g, '');}
 
   return (
     <View>
       <ImageBackground source={require('../../img/Background.jpg')} style={styles.background}>
         {/* TODO input text login, name, password, birthDate, gender */}
         <View style={styles.container_inputs}>
+
+          <FilledTextField
+            onChangeText={(login) => setLogin(login)}
+            label='Nome'
+            // keyboardType= 'phone-pad'
+            autoCapitalize= 'none'
+            autoCorrect= {false}
+            returnKeyType= 'next'
+
+          />
           <Text style={ styles.text }> Nome: </Text>
           <TextInput style={styles.input}
           onChangeText={(name) => setName(name)}
@@ -68,12 +98,28 @@ const Profile = () => {
             // ref={(input) => this.passwordInput = input}
           />
 				</View>
-        {/* TODO input text com a mascara de data */}
 
-        <View style={styles.container}>
-        <RadioButton PROP={PROP} />
-        </View>
+        <Text style={ styles.text }>Radio Button</Text>
+
+        <RadioButton
+          value="first"
+          status={gender === 'first' ? 'checked' : 'unchecked'}
+          onPress={(gender) =>{ setGender('first')}}
+        />
+        <RadioButton
+          value="second"
+          status={gender === 'second' ? 'checked' : 'unchecked'}
+          onPress={(gender) =>{ setGender('second')}}
+        />
+        <RadioButton
+          value="third"
+          status={gender === 'third' ? 'checked' : 'unchecked'}
+          onPress={(gender) =>{ setGender('third')}}
+        />
+
+        <Text style={ styles.text }>Datetime</Text>
         {/* TODO botao logout funcionar */}
+        {/* TODO botao atualizar funcionar */}
         <TouchableOpacity style={styles.button} >
           <Text> Logout </Text>
         </TouchableOpacity>
